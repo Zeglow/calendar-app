562f952b9222cee5b429b6a020551cf9774a4e38
Zeglow <zeglow2023@gmail.com>
2025-11-14 12:08:10 -0800

    add tests for CalendarListnertest

--- src/test/java/org/example/CalendarListenerTest.java
+++ src/test/java/org/example/CalendarListenerTest.java
@@ -29,4 +29,76 @@ public class CalendarListenerTest {
     }
   }
 
+  @Test
+  public void testListenerCalledOnAddEvent() {
+    Calendar calendar = new Calendar("Test");
+    TestListener listener = new TestListener();
+
+    calendar.addCalendarListener(listener);
+
+    Event event = new Event("Meeting",
+        LocalDate.of(2025, 11, 14),
+        LocalDate.of(2025, 11, 14));
+    calendar.addEvent(event);
+
+    assertEquals(1, listener.addCount);
+    assertEquals("Meeting", listener.lastAdded.getSubject());
+  }
+
+  @Test
+  public void testListenerCalledOnModifyEvent() {
+    Calendar calendar = new Calendar("Test");
+    TestListener listener = new TestListener();
+
+    calendar.addCalendarListener(listener);
+
+    Event oldEvent = new Event("Meeting",
+        LocalDate.of(2025, 11, 14),
+        LocalDate.of(2025, 11, 14));
+    calendar.addEvent(oldEvent);
+
+    Event newEvent = new Event("Updated Meeting",
+        LocalDate.of(2025, 11, 15),
+        LocalDate.of(2025, 11, 15));
+    calendar.updateEvent(oldEvent, newEvent);
+
+    assertEquals(1, listener.modifyCount);
+    assertEquals("Updated Meeting", listener.lastModified.getSubject());
+  }
+
+  @Test
+  public void testMultipleListenersCalled() {
+    Calendar calendar = new Calendar("Test");
+    TestListener listener1 = new TestListener();
+    TestListener listener2 = new TestListener();
+    TestListener listener3 = new TestListener();
+
+    calendar.addCalendarListener(listener1);
+    calendar.addCalendarListener(listener2);
+    calendar.addCalendarListener(listener3);
+
+    calendar.addEvent(new Event("Meeting",
+        LocalDate.of(2025, 11, 15),
+        LocalDate.of(2025, 11, 15)));
+
+    assertEquals(1, listener1.addCount);
+    assertEquals(1, listener2.addCount);
+    assertEquals(1, listener3.addCount);
+  }
+
+  @Test
+  public void testRemovedListenerNotCalled() {
+    Calendar calendar = new Calendar("Test");
+    TestListener listener = new TestListener();
+
+    calendar.addCalendarListener(listener);
+    calendar.removeCalendarListener(listener);
+
+    calendar.addEvent(new Event("Meeting",
+        LocalDate.of(2025, 11, 15),
+        LocalDate.of(2025, 11, 15)));
+
+    assertEquals(0, listener.addCount);
+  }
+
 }
